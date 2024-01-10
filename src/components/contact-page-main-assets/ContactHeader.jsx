e5b18f31cc7fda9b848f0b599cc804d07bc9579b
Bryan Li <76718507+Zy8712@users.noreply.github.com>
2024-01-10 05:49:07 +0000

    Reverted SSR. Caused site to 404. Other changes remain

--- src/components/contact-page-main-assets/ContactHeader.jsx
+++ src/components/contact-page-main-assets/ContactHeader.jsx
@@ -5,7 +5,7 @@ function ContactHeader() {
         <>
             <div className="w-full flex flex-col justify-center items-center mb-8">
                 <h2 className="text-gradient-blue font-semibold uppercase">Contact</h2>
-                <h1 className="text-white text-7xl mx-6 text-center uppercase"></h1>
+                <h1 className="text-white text-7xl mx-6 text-center uppercase">Contact Me</h1>
             </div>
         </>
     );
